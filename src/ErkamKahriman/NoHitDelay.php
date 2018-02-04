868398a8b319fd8bdba2940e5944a90d6e30bb35
Erkam Kahriman <erkamkahriman@gmail.com>
2018-02-04 13:32:24 +0100

    Update
    - path
    - author
    - api

--- src/ErkamKahriman/NoHitDelay.php
+++ src/ErkamKahriman/NoHitDelay.php
@@ -1,16 +1,17 @@
 <?php
 
-namespace erkam2002;
+namespace ErkamKahriman;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\utils\TextFormat as C;
 
 class NoHitDelay extends PluginBase implements Listener {
 
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getLogger()->info("Aktiviert.");
+        $this->getLogger()->info(C::GREEN."Aktiviert.");
     }
 
     public function onDamage(EntityDamageEvent $event){
@@ -19,5 +20,9 @@ class NoHitDelay extends PluginBase implements Listener {
 
     public function onHit(EntityDamageEvent $event){
         $event->setCancelled(false);
+    }
+
+    public function onDisable(){
+        $this->getLogger()->info(C::RED."Deaktiviert.");
     }
 }
